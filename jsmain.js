5033ec6ac13ffe1c7f03a1e80ebb9eeea103d42c
NedOSullivan <82614972+NedOSullivan@users.noreply.github.com>
2021-12-21 13:27:46 +0000

    Added left and right arrows and pause/play button

--- jsmain.js
+++ jsmain.js
@@ -1,5 +1,7 @@
 var pictureIndex = 0; 
 
+//Image Carousel
+
 function changeImage(n) { 
     var i; 
     var pictures = document.getElementsByClassName("images"); 
@@ -9,9 +11,19 @@ function changeImage(n) {
     pictureIndex++; 
     if (pictureIndex > pictures.length) {pictureIndex = 1} 
     pictures[pictureIndex-1].style.display = "block"; 
+    var myTimer = setInterval(changeImage, 3000, 1); //added in lines here but not working and introduced bugs
+    var pausePlayBtn = document.getElementById("pause-play-timer");
+    if (pausePlayBtn.classList.contains("play-timer")) {
+    clearInterval(myTimer);
+    myTimer = setInterval(changeImage, 3000, 1);
+}
 }
 
+//Change Image Every 4000ms
+
 setInterval(changeImage, 4000); 
+
+//Keyboard Arrow Key Functionality
 
 document.addEventListener('keydown', function(event) { 
     if(event.key == "ArrowLeft") { 
@@ -21,3 +33,16 @@ document.addEventListener('keydown', function(event) {
         changeImage(1)
     }
 });
+
+function pausePlayTimer() { //added in this function but has caused bugs
+clearInterval(myTimer);
+var pausePlayBtn = document.getElementById("pause-play-timer");
+pausePlayBtn.classList.toggle("play-timer");
+pausePlayBtn.classList.contains("play-timer") ? myTimer = setInterval(changeImage, 3000, 1) : clearInterval(myTimer);
+pausePlayBtn.classList.contains("play-timer") ? pausePlayBtn.innerHTML = "&#9646;&#9646;" : pausePlayBtn.innerHTML = "&#9654;";
+}
+
+
+
+
+
